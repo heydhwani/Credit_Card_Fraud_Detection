f0ced527b981f22b91970015139ee965a8f8548f
Dhwani <dhwanijain731@gmail.com>
2025-10-11 17:06:44 +0530

    loaded dataset

--- credit_card_fraud_detection.py
+++ credit_card_fraud_detection.py
@@ -1,1 +1,14 @@
-import pandas
+import pandas as pd
+import numpy as np
+import lightgbm as lgb
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report, roc_auc_score, confusion_matrix, roc_curve, auc
+from imblearn.over_sampling import SMOTE
+from geopy.distance import geodesic
+import joblib
+
+df = pd.read_csv('Credit_card_fraud_dataset.csv')
+print(df)
