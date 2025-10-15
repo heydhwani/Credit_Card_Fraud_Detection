f422a5702b12101b671a15bfb5fb7d357008fcb4
Dhwani <dhwanijain731@gmail.com>
2025-10-15 21:38:33 +0530

    updating data

--- credit_card_fraud_detection.py
+++ credit_card_fraud_detection.py
@@ -1,14 +1,18 @@
-import pandas as pd
-import numpy as np
-import lightgbm as lgb
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import classification_report, roc_auc_score, confusion_matrix, roc_curve, auc
-from imblearn.over_sampling import SMOTE
-from geopy.distance import geodesic
+import pandas as pd                       # data manipulation
+import numpy as np                        # numeric ops
+import matplotlib.pyplot as plt           # plotting
+import seaborn as sns                     # nicer plots (optional)
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (confusion_matrix, classification_report, roc_auc_score, roc_curve, precision_recall_curve,average_precision_score)
+from sklearn.pipeline import Pipeline
+from imblearn.over_sampling import SMOTE   # handle imbalance
 import joblib
 
 df = pd.read_csv('Credit_card_fraud_dataset.csv')
 print(df)
+
+
+
