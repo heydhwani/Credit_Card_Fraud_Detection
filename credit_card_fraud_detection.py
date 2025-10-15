613a0cee88c39de7f92a45a048a2a479b960ba2e
Dhwani <dhwanijain731@gmail.com>
2025-10-15 22:27:48 +0530

    splitting data into training and testing

--- credit_card_fraud_detection.py
+++ credit_card_fraud_detection.py
@@ -24,5 +24,16 @@ sns.countplot(x=TARGET, data=df)
 plt.title("Target distribution")
 plt.show()
 
+X = df.drop(columns=[TARGET])
+y = df[TARGET]
+
+X = pd.get_dummies(X, drop_first=True)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y)
+
+
+
+
 
 
